1fb702e371ee1ec0034edad080655e4c0fd5c9f8
gokapture <gokapture@gmail.com>
2023-09-26 08:59:11 +0530

    Add project files.

---
+++ BookReviewWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookReviewWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
